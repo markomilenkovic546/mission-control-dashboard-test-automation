8744f0ac34ea1ec795f8bcc276883637109fd3ef
markomilenkovic546 <markomilenkovic546@gmail.com>
2024-02-07 18:00:15 +0100

    updated cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     chromeWebSecurity: false,
+    specPattern: "cypress/specs/**/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require("cypress-mochawesome-reporter/plugin")(on);
