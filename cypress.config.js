51788e746845a0c66eba9235a2b632b97e507393
markomilenkovic546 <markomilenkovic546@gmail.com>
2024-02-07 17:39:17 +0100

    created LaunchPage class

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
+    baseUrl: "http://localhost:3000/",
   },
 });
---
+++ cypress/pages/launch-page.ts
@@ -0,0 +1,71 @@
+class LaunchPage {
+  elements: {
+    headerNavigation: {
+      launchLink: () => Cypress.Chainable<JQuery<HTMLElement>>;
+      upcomingLink: () => Cypress.Chainable<JQuery<HTMLElement>>;
+      historyLink: () => Cypress.Chainable<JQuery<HTMLElement>>;
+    };
+
+    missionForm: {
+      launchDateInput: () => Cypress.Chainable<JQuery<HTMLElement>>;
+      missionNameInput: () => Cypress.Chainable<JQuery<HTMLElement>>;
+      rocketTypeInput: () => Cypress.Chainable<JQuery<HTMLElement>>;
+      destinationExpoplanetDropdown: () => Cypress.Chainable<JQuery<HTMLElement>>;
+      launchMissionButton: () => Cypress.Chainable<JQuery<HTMLElement>>;
+    };
+  };
+
+  actions: {
+    headerNavigation: {
+      clickOnLaunchLink: () => void;
+      clickOnUpcomingLink: () => void;
+      clickOnHistoryLink: () => void;
+    };
+
+    missionForm: {
+      typeLaunchDateInput: (date: string) => void;
+      typeMissionNameInput: (missionName: string) => void;
+      typeRocketTypeInput: (rocketType: string) => void;
+      selectDestinationExpoplanetDropdown: (option: string) => void;
+      clearMissionNameInput: () => void;
+      clearRocketTypeInput: () => void;
+      clickOnLaunchMissionButton: () => void;
+    };
+  };
+
+  constructor() {
+    this.elements = {
+      headerNavigation: {
+        launchLink: () => cy.get('.Header-link-0-1-22[href="/launch"]'),
+        upcomingLink: () => cy.get('.Header-link-0-1-22[href="/upcoming"]'),
+        historyLink: () => cy.get('.Header-link-0-1-22[href="/history"]'),
+      },
+
+      missionForm: {
+        launchDateInput: () => cy.get("#launch-day"),
+        missionNameInput: () => cy.get("#mission-name"),
+        rocketTypeInput: () => cy.get("#rocket-name"),
+        destinationExpoplanetDropdown: () => cy.get("#planets-selector"),
+        launchMissionButton: () => cy.get('button:contains("Launch Mission ✔")'),
+      },
+    };
+    this.actions = {
+      headerNavigation: {
+        clickOnLaunchLink: () => this.elements.headerNavigation.launchLink().click(),
+        clickOnUpcomingLink: () => this.elements.headerNavigation.upcomingLink().click(),
+        clickOnHistoryLink: () => this.elements.headerNavigation.historyLink().click(),
+      },
+      missionForm: {
+        typeLaunchDateInput: (date) => this.elements.missionForm.launchDateInput().type(date),
+        typeMissionNameInput: (missionName) => this.elements.missionForm.missionNameInput().type(missionName),
+        typeRocketTypeInput: (rocketType) => this.elements.missionForm.rocketTypeInput().type(rocketType),
+        selectDestinationExpoplanetDropdown: (option) => this.elements.missionForm.destinationExpoplanetDropdown().select(option),
+        clearMissionNameInput: () => this.elements.missionForm.missionNameInput().clear(),
+        clearRocketTypeInput: () => this.elements.missionForm.rocketTypeInput().clear(),
+        clickOnLaunchMissionButton: () => this.elements.missionForm.launchMissionButton().click(),
+      },
+    };
+  }
+}
+
+export default LaunchPage;
