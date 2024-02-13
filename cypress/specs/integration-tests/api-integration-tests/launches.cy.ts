f21b0f84c17d11729b686c7cbf2c96435878542f
markomilenkovic546 <markomilenkovic546@gmail.com>
2024-02-13 15:17:36 +0100

    created positive api tests related to posting launches

--- cypress/specs/integration-tests/api-integration-tests/launches.cy.ts
+++ cypress/specs/integration-tests/api-integration-tests/launches.cy.ts
@@ -1,6 +1,9 @@
 const tv4 = require('tv4');
 import * as launches from '../../../../db/seed-data/launches-seed-data.json';
 import * as launchesSchema from '../../../fixtures/json-schemas/getMissions-response-schema.json';
+import * as postLaunchSchema from '../../../fixtures/json-schemas/postMission-response-schema.json';
+import * as launchesData from '../../../../db/seed-data/launches-seed-data.json';
+import { createLaunchPayload } from 'cypress/utils';
 
 describe('Tests which cover launches retrieving', () => {
     before(() => {
@@ -31,6 +34,79 @@ describe('Tests which cover launches retrieving', () => {
             url: `${Cypress.env('apiBaseUrl')}/launches`
         }).then((response) => {
             const isValid = tv4.validate(response.body, launchesSchema);
+            expect(isValid).to.be.true;
+        });
+    });
+});
+
+describe('Tests which cover launch posting', () => {
+    before(() => {
+        cy.task('resetDbState');
+    });
+
+    it('Response status is 201 when posting launch', () => {
+        cy.api({
+            method: 'POST',
+            url: `${Cypress.env('apiBaseUrl')}/launches`,
+            body: createLaunchPayload()
+        }).then((response) => {
+            expect(response.status).to.eq(201);
+        });
+    });
+
+    it('Create launch', () => {
+        cy.task('resetDbState');
+        const payload = createLaunchPayload();
+        cy.api({
+            method: 'POST',
+            url: `${Cypress.env('apiBaseUrl')}/launches`,
+            body: payload
+        }).then((response) => {
+            expect(response.status).to.eq(201);
+            // Get all posts and filter posted one
+            cy.api({
+                method: 'GET',
+                url: `${Cypress.env('apiBaseUrl')}/launches`
+            }).then((getLaunchesResponse) => {
+                const postedLaunch = getLaunchesResponse.body.filter(
+                    (launch) => launch.mission === payload.mission
+                );
+                // Verify that single launch instance is created
+                expect(postedLaunch.length).to.be.within(1, 1);
+            });
+        });
+    });
+
+    it('Response body expected properties when posting launch', () => {
+        cy.task('resetDbState');
+        const payload = createLaunchPayload();
+        cy.api({
+            method: 'POST',
+            url: `${Cypress.env('apiBaseUrl')}/launches`,
+            body: payload
+        }).then((response) => {
+            expect(response.status).to.eq(201);
+            expect(response.body).to.deep.eq({
+                launchDate: payload.launchDate,
+                mission: payload.mission,
+                rocket: payload.rocket,
+                target: payload.target,
+                success: true,
+                upcoming: true,
+                customers: ['NASA'],
+                flightNumber: launchesData.length + 1
+            });
+        });
+    });
+
+    it('Response body matches the expected schema when posting launches', () => {
+        const payload = createLaunchPayload();
+        cy.api({
+            method: 'POST',
+            url: `${Cypress.env('apiBaseUrl')}/launches`,
+            body: payload
+        }).then((response) => {
+            const isValid = tv4.validate(response.body, postLaunchSchema);
             expect(isValid).to.be.true;
         });
     });
--- cypress/utils.ts
+++ cypress/utils.ts
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker';
+
 //Format date
 export const formatDate = (dateString: string) => {
     const date: Date = new Date(dateString);
@@ -20,6 +22,17 @@ export const getCurrentDate = () => {
     const month = String(currentDate.getUTCMonth() + 1).padStart(2, '0');
     const day = String(currentDate.getUTCDate()).padStart(2, '0');
     return `${year}-${month}-${day}T00:00:00.000Z`;
-}
+};
+
+export const createLaunchPayload = () => {
+    const launch = {
+        launchDate: getCurrentDate(),
+        mission: faker.lorem.word(),
+        rocket: faker.lorem.word(),
+        target: 'Kepler-442 b'
+    };
+    return launch
+};
 
 
+console.log(createLaunchPayload())
